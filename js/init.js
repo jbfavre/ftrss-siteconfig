de969ef17614238d0d28339749caec9530bb92b4
Jean Baptiste Favre <github@jbfavre.org>
2014-12-24 12:42:10 +0100

    Add event listener in main page for CSS toggle. Add emitter in iframe. Update iframe sandboxing parameters to enable scripting

--- js/init.js
+++ js/init.js
@@ -11,3 +11,22 @@ $(function(){
 	window.ffdomoutline = myDomOutline;
 	myDomOutline.start();
 });
+function listener(event) {
+  if (event.data.name == 'toggle_css') {
+    $('link[rel~="stylesheet"]').prop('disabled', event.data.disabled);
+    window.ffdomoutline.element = window.ffdomoutline.selected;
+    window.ffdomoutline.updateOutlinePosition('click');
+    return false;
+  }else{
+    console.log('Unknown message received by main page listener');
+    console.log(event.data);
+    console.log('Discarded...');
+  }
+}
+if (document.addEventListener){
+  console.log ('addEventListener');
+  addEventListener("message", listener, false)
+} else {
+  console.log('attachEvent');
+  attachEvent("onmessage", listener)
+}
